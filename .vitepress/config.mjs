68776fea3fb1341f57928694cb96b84b26466610
jnavarro30 <jnavarro30@live.com>
2025-04-02 13:46:01 -0700

    build project

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "independa-docs",
+  base: "/independa-docs",
   title: "Documentation",
   description: "For the Consumer, Enterprise & Developer",
   themeConfig: {
