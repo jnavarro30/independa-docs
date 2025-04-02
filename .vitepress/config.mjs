1fbb8b439b1add4b70d89c8bf11657d9327a589c
jnavarro30 <jnavarro30@live.com>
2025-04-02 13:44:58 -0700

    add base for ghpages

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "independa-docs",
   title: "Documentation",
   description: "For the Consumer, Enterprise & Developer",
   themeConfig: {
